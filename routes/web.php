dd5f570b158197838fc0cebf24bd1f75d9961376
Zisis Koukoumakis <koykis@gmail.com>
2019-03-05 01:53:45 +0200

    Database works as intended. Login is now required to enter the webiste. Home view needs some work...

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('Auth\Login');
+})->middleware('auth');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
